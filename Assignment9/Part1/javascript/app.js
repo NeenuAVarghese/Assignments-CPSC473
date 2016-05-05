97cb1007f0016129fff968ef7dce4a94611bde31
NeenuAVarghese <neenuavarghese@gmail.com>
2016-05-04 19:25:42 -0700

    Part 1 of assignment 9

--- Assignment9/Part1/javascript/app.js
+++ Assignment9/Part1/javascript/app.js
@@ -1,17 +1,24 @@
+// Client-side code
+/* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, undef: true, unused: true, strict: true, trailing: true */
+
+// Server-side code
+/* jshint node: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, undef: true, unused: true, strict: true, trailing: true */
+/*globals ko*/
+"use strict";
 var commentModel = function(){
   //for input box
   this.newComment =  ko.observable("");
 
   // initial state of comments array
   this.commentsArray = ko.observableArray([
-      {cmt: 'First Comment'},
-      {cmt: 'Second Comment'}
+      {cmt: "First Comment"},
+      {cmt: "Second Comment"}
     ]);
 
   //handles add button action
    this.addComment = function() {
-      if(this.newComment() != ""){
-        commentsArray.push({cmt: this.newComment()});
+      if(this.newComment() !== ""){
+        this.commentsArray.push({cmt: this.newComment()});
         this.newComment("");
       } 
     };
